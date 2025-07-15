1301192b8c2b754d30577c908ebdb51ca39f6ee1
NicoleAndreaBolus <nagbolus.student@ua.edu.ph>
2025-07-15 09:55:12 +0800

    Updated users end

---
+++ session14/task-app/src/pages/Tasks.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+import { useAuth } from '../AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+
+export default function Tasks() {
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    
+  useEffect(() => {
+     if(!user){
+      navigate("/login");
+    }else{
+        document.title = `${user.fname.toUpperCase()} ${user.lname.toUpperCase()} - Tasks`;
+    }
+  }, [user, navigate])
+
+  return (
+    <Container fluid className='m-1 m-lg-5 border rounded-4 p-1 p-lg-4 d-flex flex-column align-items-center justify-content-center'>
+        <h1 className='my-3 display-3 fw-bold text-primary'>Welcome to Tasks Page!</h1>
+    </Container>
+  )
+}
